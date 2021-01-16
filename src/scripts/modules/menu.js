c44e6da43f3e81921d48424c11fca5e1cfe72ef8
Aldemir Souza <aldemmir17b@hotmail.com>
2021-01-15 23:47:06 -0300

    Update header menu

--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -7,26 +7,36 @@ const menu = {
             url: `/api/catalog_system/pub/category/tree/${tree}`
         }
         const callbackMenu = data =>{
-            menu.render({data, mainClass});
+            menu.render({data, main: mainClass});
         }
 
         api(config, callbackMenu);
     },
 
-    render: ({data, mainClass}) =>{
-        const $mainClass = document.querySelector(mainClass);
-        data.forEach(category => {
-            const {name, url} = category; 
+    render: ({data, main}) =>{
+        const $mainClass = typeof main == 'string' ? document.querySelector(main): main;
+        const mainClassWithoutPoint = typeof main == 'string' ? main.replace('.', ''): main.classList[0].replace('.', '');
 
-            const newCategory = document.createElement('li');
-            //newCategory.classList.add(`${mainClass}-item`);
+        data.forEach(category => {
+            const {name, url, hasChildren, children} = category; 
+
+            const newCategory = document.createElement('div');
+            newCategory.classList.add(`${mainClassWithoutPoint}-item`);
 
             const newCategoryLink = document.createElement('a');
             newCategoryLink.href = url;
             newCategoryLink.textContent = name;
 
+            const newCategoryItem = document.createElement('div');
+            newCategoryItem.classList.add(`${mainClassWithoutPoint}-box-category`);
+
             newCategory.appendChild(newCategoryLink);
+            newCategory.appendChild(newCategoryItem);
             $mainClass.appendChild(newCategory);
+
+            if(hasChildren){
+                menu.render({data: children, main: newCategoryItem});
+            }
         })
     }
 }
